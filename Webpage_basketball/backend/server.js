d776d435e3c853df5c57fee2fdc0289bdc35d3e5
Bashir-AK <bashira.k@hotmail.com>
2022-09-29 19:42:34 +0200

    start

--- Webpage_basketball/backend/server.js
+++ Webpage_basketball/backend/server.js
@@ -9,6 +9,17 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
+var sqlite3 = require('sqlite3').verbose();
+var file = "play_info_database";
+var db = new sqlite3.Database(file);
+db.all("SELECT * FROM Player_data", function(err, rows) {
+  rows.forEach(function (row) {
+      console.log(row.player_name);
+  })
+});	
+db.close();
+
+
 server.listen(port, hostname, () => {
   console.log("Server running at http://${hostname}:${port}/");
 });
